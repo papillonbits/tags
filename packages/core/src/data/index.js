0a53af63ec125dd598320807c1777809e3c6e662
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2022-10-23 13:24:51 +0200

    implement data

---
+++ packages/core/src/data/index.js
@@ -0,0 +1,50 @@
+// sample data for the multi tag app
+
+export const telephones = {
+  title: 'Telephone numbers',
+  description: 'Enter telephone numbers.',
+}
+
+export const intro = {
+  title: 'Tags',
+  description:
+    'Enter multiple email addresses using tags. Tags are completed with a tab or comma. A tab after the last tag jumps to the next element on the page what can get focus. Tags can be removed with backspace or by clicking on them. The input field grows to multiple lines.',
+}
+
+export const emails = {
+  title: 'Email addresses',
+  items: [
+    {
+      id: 0,
+      text: 'me@me.com',
+    },
+    {
+      id: 1,
+      text: 'ali@baba.com',
+    },
+    {
+      id: 2,
+      text: 'someone@amazon.com',
+    },
+    {
+      id: 3,
+      text: 'one@abc.com',
+    },
+    {
+      id: 4,
+      text: 'two@abc.com',
+    },
+    {
+      id: 5,
+      text: 'three@abc.com',
+    },
+    {
+      id: 6,
+      text: 'four@abc.com',
+    },
+    {
+      id: 7,
+      text: 'five@abc.com',
+    },
+  ],
+}
